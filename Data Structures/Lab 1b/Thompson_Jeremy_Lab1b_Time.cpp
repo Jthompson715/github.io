0a1a20ae5400a1bb9c3724b1340208c518590ed8
Jeremy Thompson <Jeremy1967@yahoo.com>
2015-08-10 00:11:12 -0700

    Time.cpp

---
+++ Data Structures/Lab 1b/Thompson_Jeremy_Lab1b_Time.cpp
@@ -0,0 +1,43 @@
+// Time.cpp : Defines the entry point for the console application.
+//"Lab 1b, The \"Road.h\" Program \n";
+//"Programmer: JEREMY THOMPSON\n";
+//"Editor(s) used: JNotePad\n";
+//"Compiler(s) used: CodeBlocks\n";
+
+#include "Thompson_Jeremy_Lab1b_Time.h"
+
+void Time::setHours(double hour)
+{
+  hours = hour;
+}
+
+void Time::setMinutes(double min)
+{
+  minutes = min;
+}
+
+void Time::setSeconds(double second)
+{
+  seconds = second;
+}
+
+double Time::timeInHours() const
+{
+  double h = 0;
+  h = (hours + (minutes) / 60 + (seconds) / 3600);
+  return h;
+}
+
+double Time::timeInMinutes() const
+{
+  double m = 0;
+  m = minutes + ((hours * 60) + (seconds) / 60);
+  return m;
+}
+
+double Time::timeInSeconds() const
+{
+  double s = 0;
+  s = ((hours * 60 * 60) + (minutes * 60) + seconds);
+  return s;
+}
